a87848ad5d4487d9d6d88a3e60ca9b5de259afec
Julie Theriault <joules2319@gmail.com>
2020-02-02 14:18:25 -0700

    First set of tests passed, Employee class functional

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,8 +1,28 @@
 class Employee {
-	constructor(name, id, title)
+	constructor(name, id, email) {
+		this.name = name;
+		this.id = id;
+		this.email = email;
+		this.role = "Employee";
+	};
+
+	getName() {
+		return this.name;
+	}
+	getId() {
+		return this.id;
+	}
+	getEmail() {
+		return this.email;
+	}
+	getRole() {
+		return this.role;
+	}
 	printInfo() {
 		for (var key in this) {
 			console.log(`${key}: ${this[key]}`);
 		}
 	}
 }
+
+module.exports = Employee;
