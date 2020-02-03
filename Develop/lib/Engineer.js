a0929d2a2a7fcf3d89d4e6a171967a44291ffaf1
Julie Theriault <joules2319@gmail.com>
2020-02-02 19:38:58 -0700

    added Intern class, passes tests

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,8 +1,8 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-	constructor(name, id, email, github, title) {
-		super(name, id, email, title);
+	constructor(name, id, email, github) {
+		super(name, id, email);
 		this.github = github;
 		this.role = "Engineer"
 	}
