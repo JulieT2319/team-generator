f2e06668d486b41d14c512c046e3db27cfb0c2c9
Julie T <47932789+JulieT2319@users.noreply.github.com>
2020-02-04 11:21:29 -0700

    added email validation to inquirer function

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-//node library dependencies
+//node libraries
 const fs = require("fs");
 const inquirer = require("inquirer");
-//app library dependencies
-const Engineer = require("./Develop/lib/Engineer");
-const Manager = require("./Develop/lib/Manager");
-const Intern = require("./Develop/lib/Intern");
+//app libraries
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+
+//app templates
+
+//control variables
+var needManager = true;
+var needEngineer = true;
+var needIntern = true;
 
 //employee arrays
 const manager = [];
@@ -13,29 +20,40 @@ const interns = [];
 
 //employee information collection functions
 function addEngineer() {
-	inquirer.prompt([{
-		type: "input",
-		name: "name",
-		message: "What is the engineer's name?"
-	},
-	{
-		type: "input",
-		name: "email",
-		message: "What is the engineer's email address?"
-	},
-	{
-		type: "input",
-		message: "What is the engineer's ID?",
-		name: "id"
-	},
-	{
-		type: "input",
-		name: "gitHub",
-		message: "What is the engineer's github username?"
-	}]).then(function (data) {
-		engineers.push(new Engineer(data.name, data.id, data.email, data.gitHub))
-		console.log(engineers);
-	})
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email address?",
+        validate: function(value) {
+          if (value.includes("@") && value.includes(".")) {
+            return true;
+          } else {
+            return "Please provide a valid email address.";
+          }
+        }
+      },
+      {
+        type: "input",
+        message: "What is the engineer's ID?",
+        name: "id"
+      },
+      {
+        type: "input",
+        name: "gitHub",
+        message: "What is the engineer's github username?"
+      }
+    ])
+    .then(function(data) {
+      engineers.push(new Engineer(data.name, data.id, data.email, data.gitHub));
+      console.log(engineers);
+    });
 }
 
 addEngineer();
