3da5e813330f97a34416944d48641a591d8b5e9b
Julie Theriault <joules2319@gmail.com>
2020-02-03 18:27:52 -0700

    added function to add engineer to team

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+//node library dependencies
 const fs = require("fs");
 const inquirer = require("inquirer");
+//app library dependencies
+const Engineer = require("./Develop/lib/Engineer");
+const Manager = require("./Develop/lib/Manager");
+const Intern = require("./Develop/lib/Intern");
 
-function addEngineer{
+//employee arrays
+const manager = [];
+const engineers = [];
+const interns = [];
+
+//employee information collection functions
+function addEngineer() {
 	inquirer.prompt([{
 		type: "input",
 		name: "name",
@@ -21,5 +32,10 @@ function addEngineer{
 		type: "input",
 		name: "gitHub",
 		message: "What is the engineer's github username?"
-	}])
-} 
+	}]).then(function (data) {
+		engineers.push(new Engineer(data.name, data.id, data.email, data.gitHub))
+		console.log(engineers);
+	})
+}
+
+addEngineer();
