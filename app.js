1b6d24dbac71f2f6ddac40ba8ee50800a86469ab
Julie Theriault <joules2319@gmail.com>
2020-02-05 14:50:04 -0700

    added separated html templates for employee types

--- app.js
+++ app.js
@@ -22,129 +22,133 @@ const interns = [];
 
 //add employee functions
 async function addEngineer() {
-	inquirer
-		.prompt([
-			{
-				type: "input",
-				name: "name",
-				message: "What is the engineer's name?"
-			},
-			{
-				type: "input",
-				name: "email",
-				message: "What is the engineer's email address?",
-				validate: function (value) {
-					if (value.includes("@") && value.includes(".")) {
-						return true;
-					} else {
-						return "Please provide a valid email address.";
-					}
-				}
-			},
-			{
-				type: "input",
-				message: "What is the engineer's ID?",
-				name: "id"
-			},
-			{
-				type: "input",
-				name: "gitHub",
-				message: "What is the engineer's github username?"
-			}
-		])
-		.then(function (data) {
-			engineers.push(new Engineer(data.name, data.id, data.email, data.gitHub));
-			console.log(engineers);
-		});
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email address?",
+        validate: function(value) {
+          if (value.includes("@") && value.includes(".")) {
+            return true;
+          } else {
+            return "Please provide a valid email address.";
+          }
+        }
+      },
+      {
+        type: "input",
+        message: "What is the engineer's ID?",
+        name: "id"
+      },
+      {
+        type: "input",
+        name: "gitHub",
+        message: "What is the engineer's github username?"
+      }
+    ])
+    .then(function(data) {
+      engineers.push(new Engineer(data.name, data.id, data.email, data.gitHub));
+      console.log(engineers);
+    });
 }
 
 async function addManager() {
-	inquirer
-		.prompt([
-			{
-				type: "input",
-				name: "name",
-				message: "What is the manager's name?"
-			},
-			{
-				type: "input",
-				name: "email",
-				message: "What is the manager's email address?",
-				validate: function (value) {
-					if (value.includes("@") && value.includes(".")) {
-						return true;
-					} else {
-						return "Please provide a valid email address.";
-					}
-				}
-			},
-			{
-				type: "input",
-				message: "What is the manager's ID?",
-				name: "id"
-			},
-			{
-				type: "input",
-				name: "office",
-				message: "What is the manager's office number?"
-			}
-		])
-		.then(function (data) {
-			manager.push(new Manager(data.name, data.id, data.email, data.office));
-			console.log(manager);
-		});
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email address?",
+        validate: function(value) {
+          if (value.includes("@") && value.includes(".")) {
+            return true;
+          } else {
+            return "Please provide a valid email address.";
+          }
+        }
+      },
+      {
+        type: "input",
+        message: "What is the manager's ID?",
+        name: "id"
+      },
+      {
+        type: "input",
+        name: "office",
+        message: "What is the manager's office number?"
+      }
+    ])
+    .then(function(data) {
+      manager.push(new Manager(data.name, data.id, data.email, data.office));
+      console.log(manager);
+    });
 }
 
 async function addIntern() {
-	await inquirer
-		.prompt([
-			{
-				type: "input",
-				name: "name",
-				message: "What is the intern's name?"
-			},
-			{
-				type: "input",
-				name: "email",
-				message: "What is the intern's email address?",
-				validate: function (value) {
-					if (value.includes("@") && value.includes(".")) {
-						return true;
-					} else {
-						return "Please provide a valid email address.";
-					}
-				}
-			},
-			{
-				type: "input",
-				message: "What is the intern's ID?",
-				name: "id"
-			},
-			{
-				type: "input",
-				name: "school",
-				message: "What is the intern's school?"
-			}
-		]).then(function (data) {
-			interns.push(new Intern(data.name, data.id, data.email, data.school));
-			console.log(interns);
-		});
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email address?",
+        validate: function(value) {
+          if (value.includes("@") && value.includes(".")) {
+            return true;
+          } else {
+            return "Please provide a valid email address.";
+          }
+        }
+      },
+      {
+        type: "input",
+        message: "What is the intern's ID?",
+        name: "id"
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is the intern's school?"
+      }
+    ])
+    .then(function(data) {
+      interns.push(new Intern(data.name, data.id, data.email, data.school));
+      console.log(interns);
+    });
 }
 
 inquirer
-	.prompt([
-		{
-			name: "project",
-			message: "What project are you creating a team for?",
-			type: "input"
-		}
-	])
-	.then(function (data) {
-		project = data.project;
-		addIntern().then(function () {
-			console.log(`Manager needed: ${needManager}
+  .prompt([
+    {
+      name: "project",
+      message: "What project are you creating a team for?",
+      type: "input"
+    }
+  ])
+  .then(function(data) {
+    project = data.project;
+    while (needManager) {
+      addManager().then(function() {
+        needManager = false;
+        console.log(`Manager needed: ${needManager}
     Engineer(s) needed: ${needEngineer}
     Intern(s) needed: ${needIntern}
     Project name: ${project}`);
-		});
-	});
+      });
+    }
+  });
